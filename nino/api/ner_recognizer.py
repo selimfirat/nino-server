d59322724ee29c55b325d6cb017411a707f23a6e
root <yilmazselimfirat@gmail.com>
2019-04-21 19:47:21 +0000

    flair models changed with faster versions

--- nino/api/ner_recognizer.py
+++ nino/api/ner_recognizer.py
@@ -9,8 +9,8 @@ class NERRecognizer:
     
     def __init__(self):
         # ner_url = 'https://storage.googleapis.com/chakki/datasets/public/ner/model_en.zip'
-        self.ner_tagger = SequenceTagger.load('ner')
-        self.ontoner_tagger = SequenceTagger.load('ner-ontonotes')
+        self.ner_tagger = SequenceTagger.load('ner-fast') # ner-fast
+        self.ontoner_tagger = SequenceTagger.load('ner-ontonotes-fast') # ner-ontonotes-fast
 
         # download(ner_url)
         # self.ner_model = anago.Sequence.load('/home/yilmazselimfirat3/.keras/datasets/weights.h5', '/home/yilmazselimfirat3/.keras/datasets/params.json', '/home/yilmazselimfirat3/.keras/datasets/preprocessor.pickle')  
@@ -21,12 +21,12 @@ class NERRecognizer:
         sentences = nltk.sent_tokenize(text)
         for sent in sentences:
             sentence = Sentence(sent)
-            self.tagger.predict(sentence)
-            sent_tags = sentence.to_dict(tag_type='ner')
+            self.ner_tagger.predict(sentence)
+            sent_tags = sentence.to_dict(tag_type='ner-fast')
             entities.extend(sent_tags["entities"])
 
             self.ontoner_tagger.predict(sentence)
-            sent_tags = sentence.to_dict(tag_type='ner-ontonotes')
+            sent_tags = sentence.to_dict(tag_type='ner-ontonotes-fast')
             entities.extend(sent_tags["entities"])
 
         return entities
